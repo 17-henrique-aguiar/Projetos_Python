b418341b1b7238b87faae4ec04be6127bd05be74
Henrique <henriqueaguiar670@gmail.com>
2025-12-12 23:15:21 -0300

    Update app.py

--- projeto_vendas/app.py
+++ projeto_vendas/app.py
@@ -1,2 +1,17 @@
 import streamlit as st
 import pandas as pd
+
+st.set_page_config(page_title="Projeto de Análise de Vendas", layout="wide")
+
+st.title("Projeto de Análise de Vendas (Streamlit + Pandas)")
+st.write("Faça upload de um arquivo de um arquivo CSV e navegue entre as páginas para anásile.")
+
+# Upload do arquivo
+uploaded_file = st.file_uploader("Envie o arquivo CSV", type=["csv"])
+
+# Guardado no Session state para outras páginas acessarem
+if uploaded_file:
+  st.session_state["df"] = pd.read_csv(uploaded_file)
+  st.session_state("Arquivo carregado com sucesso! Acesse as abas laterais.")
+else:
+  st.warning("Envie um arquivo CSV para começar.")
